500a42fd359467a144f4852005bcb73cb76e0870
iimAtomic <vegbahermerite@gmail.com>
2023-11-15 19:14:02 +0100

    srapping

--- Scrapping/scrap.py
+++ Scrapping/scrap.py
@@ -1,4 +1,4 @@
-
+# Import des bibliothèques nécessaires
 import json
 import os
 import tkinter as tk
@@ -7,28 +7,27 @@ from bs4 import BeautifulSoup
 import requests as rq
 import matplotlib.pyplot as plt
 
+# URL de la page à analyser
 url = "https://www.darty.com/"
 response = rq.get(url)
 html_content = response.content
 
-#script pour forcer la création du fichier json dans le dossier du projet
+# Script pour forcer la création du fichier JSON dans le dossier du projet
 script_dir = os.path.dirname(os.path.abspath(__file__))
 json_path = os.path.join(script_dir, "Information.json")
 
-
-#recuperation du contenue Html 
+# Récupération du contenu HTML
 soup = BeautifulSoup(html_content, 'html.parser')
 
-#recuperation de tout les articles
+# Récupération de tous les articles
 appareils = soup.find_all('article', class_='product')
 data = []
 
-#boucle pour afficher chaque articles
+# Boucle pour afficher chaque article
 for appareil in appareils:
     avis_appareil = appareil.find('div', class_='reviews')
     if avis_appareil:
-     
-        nombre_avis_texte = avis_appareil.text.replace(',', '').strip()
+        nombre_avis_texte = avis_appareil.text.replace(',', '').strip().split()[0]
         nombre_avis = nombre_avis_texte
         print(f"Nombre d'avis: {nombre_avis}")
 
@@ -38,14 +37,12 @@ for appareil in appareils:
             print(f"Nom de l'appareil: {nom_appareil}")
             data.append({'Nom de l\'appareil': nom_appareil, 'Nombre d\'avis': nombre_avis})
             print("------")
-        with open(json_path, "w") as file:
-         json.dump(data, file)
 
 df = pd.DataFrame(data)
 
-#Affichage
+# Affichage
 plt.figure(figsize=(18, 12))
-plt.bar(df['Nom de l\'appareil'], df['Nombre d\'avis'])
+plt.bar(df['Nom de l\'appareil'], df['Nombre d\'avis'].astype(int))
 plt.xlabel('Nom de l\'appareil')
 plt.ylabel('Nombre d\'avis')
 plt.title('Nombre d\'avis par appareil')
@@ -53,11 +50,4 @@ plt.xticks(rotation=45, ha='right')
 plt.tight_layout()
 plt.show()
 
-
-
-   
-
-
 print(df)
-
-
